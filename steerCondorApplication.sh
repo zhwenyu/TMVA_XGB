2ecae7a0494f6631355fa37123016bfae0741492
Wenyu <wenyu_zhang@brown.edu>
2020-12-09 20:01:08 -0500

    steer script correct

--- steerCondorApplication.sh
+++ steerCondorApplication.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-shiftList=("JECup" "JECdown" "JERup" "JERdown")
+shiftList=("nominal" "JECup" "JECdown" "JERup" "JERdown")
+
 
 for sft in ${shiftList[@]}; do
-  echo 'submitApplication ${sft}'
+  echo "---- submitApplication ${sft} ----"
   python -u submitApplication.py ${sft}
 done
 
